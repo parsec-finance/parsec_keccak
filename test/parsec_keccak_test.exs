5539d6702bedf030450efedae7f1a36dc80c1553
Ben Sheehan <sheehan1102@gmail.com>
2023-08-29 15:28:48 -0400

    Update specs

--- test/parsec_keccak_test.exs
+++ test/parsec_keccak_test.exs
@@ -3,8 +3,10 @@ defmodule ParsecKeccakTest do
   doctest ParsecKeccak
 
   test "hash" do
+    raw  = "Transfer(address,address,uint256)"
     tfer = "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 
-    assert ParsecKeccak.hash("Transfer(address,address,uint256)") == tfer
+    assert ParsecKeccak.hash(raw) == {:ok, tfer}
+    assert {:error, _} = ParsecKeccak.hash(raw, decode: true)
   end
 end
